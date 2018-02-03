352e1ded9e695d11d850f93aef7eb8534dfb7526
Max Kobilev <mkobilev@gmail.com>
2018-02-03 15:31:09 +0300

    codereview week 1

--- week1/index.js
+++ week1/index.js
@@ -1,36 +1,48 @@
 //Ваш код будет здесь
-
-var startNewGame = function() {
-
+window.addEventListener('load', function() {
+  var button = document.querySelector('.startNewGame');
   var cells = document.querySelectorAll('.cell');
-  var winnerMsg = document.querySelector('.winner-message').innerHTML = ''
-  var clicks = [];
+  var field = document.querySelector('.field');
+  var winnerMsg = document.querySelector('.winner-message');
+  var nextMove = 'x';
 
-  cells.forEach( cell => {
-
-    cell.classList.remove('x');
-    cell.classList.remove('o');
-
-    let listener = cell.addEventListener('click', function() {
-      clicks.push(1)
-
-      if (cell.classList.value.search('x') === -1 &&  cell.classList.value.search('x') === -1 ) {
-        if (clicks.length % 2 === 0) {
-          cell.classList.add('x');
-        } else {
-          cell.classList.add('o');
-        }
-      }
-
-      console.log('getWinner', getWinner());
-      var winner = getWinner();
-      if (winner) {
-        var winnerMsg = document.querySelector('.winner-message').innerHTML = `${winner} Победил`;
-      };
-      cell.removeEventListener('click', function() {
-        console.log('removed');
-      });
+  button.addEventListener('click', function startNewGame() {
+    cells.forEach(cell => {
+      cell.classList.remove('x');
+      cell.classList.remove('o');
     });
-
+    nextMove = 'x';
+    winnerMsg.innerHTML = '';
   });
-};
+
+  field.addEventListener('click', function handleCellClick(e) {
+    if (
+      !e.target.classList.contains('cell') ||
+      e.target.classList.contains('x') ||
+      e.target.classList.contains('o')
+    ) {
+      return;
+    }
+
+    if (getWinner()) {
+      return;
+    }
+
+    e.target.classList.add(nextMove);
+    if (nextMove === 'x') {
+      nextMove = 'o';
+    } else {
+      nextMove = 'x';
+    }
+
+    var winner = getWinner();
+    if (winner) {
+      if (winner === 'x') {
+        winnerMsg.innerHTML = 'Крестик победил';
+      } else {
+        winnerMsg.innerHTML = 'Нолик победил';
+      }
+    }
+  });
+
+});
