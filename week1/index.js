14f1f5d63f0708f8bdb45b70fd6064974632a16f
Max Kobilev <mkobilev@gmail.com>
2018-02-03 14:19:45 +0300

    completd 1 week

--- week1/index.js
+++ week1/index.js
@@ -1,15 +1,36 @@
 //Ваш код будет здесь
 
 var startNewGame = function() {
-  alert('Она работает');
 
-  var cells = document.querySelectorAll('div.cell');
-  console.log('cells', cells);
+  var cells = document.querySelectorAll('.cell');
+  var winnerMsg = document.querySelector('.winner-message').innerHTML = ''
+  var clicks = [];
 
   cells.forEach( cell => {
+
     cell.classList.remove('x');
     cell.classList.remove('o');
 
-    console.log('cell', cell);
-  })
-}
+    let listener = cell.addEventListener('click', function() {
+      clicks.push(1)
+
+      if (cell.classList.value.search('x') === -1 &&  cell.classList.value.search('x') === -1 ) {
+        if (clicks.length % 2 === 0) {
+          cell.classList.add('x');
+        } else {
+          cell.classList.add('o');
+        }
+      }
+
+      console.log('getWinner', getWinner());
+      var winner = getWinner();
+      if (winner) {
+        var winnerMsg = document.querySelector('.winner-message').innerHTML = `${winner} Победил`;
+      };
+      cell.removeEventListener('click', function() {
+        console.log('removed');
+      });
+    });
+
+  });
+};
